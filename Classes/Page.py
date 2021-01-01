02f703e192eace61fb98278e40591dc056bb90dd
mega5800 <43865655+mega5800@users.noreply.github.com>
2021-01-01 21:22:57 +0200

    added image cropping

--- Classes/Page.py
+++ Classes/Page.py
@@ -3,9 +3,9 @@ import cv2
 class Page:
     def __init__(self, i_PageFilePath):
         self.__m_PageFilePath = i_PageFilePath
-        self.__convertAndCropPNGFilesToGrayScale()
+        self.__convertAndCropPNGFileToGrayScale()
 
-    def __convertAndCropPNGFilesToGrayScale(self):
+    def __convertAndCropPNGFileToGrayScale(self):
         imgToCropAndConvert = cv2.imread(self.__m_PageFilePath)
         imgToCropAndConvert = cv2.cvtColor(imgToCropAndConvert, cv2.COLOR_BGR2GRAY)
         imgToCropAndConvert = imgToCropAndConvert[835:3870, 0:2979]
