1bdd9ea0ab7660f5871e55fad8bb0baf378e6927
mega5800 <43865655+mega5800@users.noreply.github.com>
2021-01-09 23:03:23 +0200

    WordCropper.py was improved

--- Croppers/WordCropper.py
+++ Croppers/WordCropper.py
@@ -13,24 +13,13 @@ class WordCropper(Cropper):
 
         return self.__m_WordsList
 
-    # יש כפל קוד!!!!
-    # לבצע חלוקה נכונה למתודות במקום לשכפל קוד
     def __cropWordsFromPage(self):
         self.__m_WordsList = []
+        thresh = self.__getThreshValue()
+        morph = self.__performStructuringElementAndGetMorphValue(thresh)
+        self.__getSortedContoursList(morph)
 
-        img = self.__m_LineImageToCrop.copy()
-        thresh = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (48, 46))
-        morph = cv2.morphologyEx(thresh, cv2.MORPH_DILATE, kernel)
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (42, 87))
-        morph = cv2.morphologyEx(morph, cv2.MORPH_OPEN, kernel)
-
-        contoursList = cv2.findContours(morph, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        contoursList = contoursList[0] if len(contoursList) == 2 else contoursList[1]
-        contoursList = self.__sortContoursList(contoursList)
-
-        for c in contoursList:
+        for c in self.__m_ContoursList:
             box = cv2.boundingRect(c)
             x, y, w, h = box
             wordToCrop = self.__m_LineImageToCrop[y:y+h, x:x+w]
@@ -40,29 +29,37 @@ class WordCropper(Cropper):
             self.__m_WordsList.append(Word(self.__m_NumberOfWordsInLine, wordFolderPath, wordFilePath))
             cv2.imwrite(wordFilePath, wordToCrop)
 
+    def __getThreshValue(self):
+        img = self.__m_LineImageToCrop.copy()
+        thresh = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+        return thresh
+
+    def __performStructuringElementAndGetMorphValue(self, i_ThresholdValue):
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (48, 46))
+        morph = cv2.morphologyEx(i_ThresholdValue, cv2.MORPH_DILATE, kernel)
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (42, 87))
+        morph = cv2.morphologyEx(morph, cv2.MORPH_OPEN, kernel)
+
+        return morph
+
+    def __getSortedContoursList(self, i_MorphValue):
+        self.__m_ContoursList = cv2.findContours(i_MorphValue, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        self.__m_ContoursList = self.__m_ContoursList[0] if len(self.__m_ContoursList) == 2 else self.__m_ContoursList[1]
+        self.__m_ContoursList = self.__sortContoursList(self.__m_ContoursList)
+
     def __sortContoursList(self, i_ContoursList):
         return sorted(i_ContoursList, key=lambda ctr: cv2.boundingRect(ctr)[0], reverse=True)
 
     def __saveContoursImage(self):
-        img = self.__m_LineImageToCrop.copy()
-        thresh = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (48, 46))
-        morph = cv2.morphologyEx(thresh, cv2.MORPH_DILATE, kernel)
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (42, 87))
-        morph = cv2.morphologyEx(morph, cv2.MORPH_OPEN, kernel)
-
-        contoursList = cv2.findContours(morph, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        contoursList = contoursList[0] if len(contoursList) == 2 else contoursList[1]
-        contoursList = self.__sortContoursList(contoursList)
-
-        result = img.copy()
-        for c in contoursList:
+        result = self.__m_LineImageToCrop.copy()
+        for c in self.__m_ContoursList:
             box = cv2.boundingRect(c)
             x, y, w, h = box
             cv2.rectangle(result, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
         cv2.imwrite(self.__m_LineImageToCropFolderPath + "/words_edges_test.png", result)
+
 
 import cv2
 from Classes.Utils import Utils
