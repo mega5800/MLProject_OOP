9d2f098ec63b92a08900fd0a2a48fea87f1076df
mega5800 <43865655+mega5800@users.noreply.github.com>
2021-02-08 21:13:58 +0200

    deleted c++ code

--- Program.py
+++ Program.py
@@ -1,8 +1,6 @@
 import winsound
 from Classes.PDFBooksFolder import PDFBooksFolder
-import pybind11module
 
-pybind11module.sayHello()
 book = PDFBooksFolder("pdf books")
 frequency = 2500
 duration = 500
