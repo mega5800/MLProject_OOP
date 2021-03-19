ff9dcc2c0e992e12a6fafd354c86455dc72f5088
mega5800 <43865655+mega5800@users.noreply.github.com>
2021-03-19 22:49:01 +0200

    temp commit

--- Program.py
+++ Program.py
@@ -1,7 +1,55 @@
 import winsound
 from Classes.PDFBooksFolder import PDFBooksFolder
 
-book = PDFBooksFolder(r"F:\Python Projects\MLProject_OOP\pdf books")
-frequency = 2500
-duration = 500
-winsound.Beep(frequency, duration)
+from keras.preprocessing import image
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input
+import numpy as np
+from sklearn.cluster import KMeans
+import os, shutil, glob, os.path
+from PIL import Image as pil_image
+
+def finishSound():
+    frequency = 2500
+    duration = 500
+    winsound.Beep(frequency, duration)
+
+#book = PDFBooksFolder(r"F:\Python Projects\MLProject_OOP\pdf books")
+
+
+image.LOAD_TRUNCATED_IMAGES = True
+model = VGG16(weights='imagenet', include_top=False)
+
+imdir = 'C:/indir/'
+targetdir = "C:/outdir/"
+number_clusters = 3
+
+# Loop over files and get features
+filelist = glob.glob(os.path.join(imdir, '*.png'))
+filelist.sort()
+featurelist = []
+for i, imagepath in enumerate(filelist):
+    print("    Status: %s / %s" %(i, len(filelist)), end="\r")
+    img = image.load_img(imagepath, target_size=(224, 224))
+    img_data = image.img_to_array(img)
+    img_data = np.expand_dims(img_data, axis=0)
+    img_data = preprocess_input(img_data)
+    features = np.array(model.predict(img_data))
+    featurelist.append(features.flatten())
+
+# Clustering
+kmeans = KMeans(n_clusters=number_clusters, random_state=0).fit(np.array(featurelist))
+
+# Copy images renamed by cluster
+# Check if target dir exists
+try:
+    os.makedirs(targetdir)
+except OSError:
+    pass
+# Copy with cluster name
+print("\n")
+for i, m in enumerate(kmeans.labels_):
+    print("    Copy: %s / %s" %(i, len(kmeans.labels_)), end="\r")
+    shutil.copy(filelist[i], targetdir + str(m) + "_" + str(i) + ".jpg")
+
+finishSound()
