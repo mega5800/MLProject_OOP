99c03f13bd9392d9742c2f52fb41c36a51324bcd
mega5800 <43865655+mega5800@users.noreply.github.com>
2021-03-26 17:08:24 +0300

    temp commit

--- Program.py
+++ Program.py
@@ -1,18 +1,34 @@
+import sys
 import winsound
 from Classes.ImageClusteringComponent import ImageClusteringComponent
 from Classes.PDFBooksFolder import PDFBooksFolder
 
+constErrorMessage = "An exception occurred\nPlease check the PDF books folder path"
 
 def finishSound():
     frequency = 2500
     duration = 500
     winsound.Beep(frequency, duration)
 
+def performAMethodInTryCatchBlock(i_MethodPointer, i_ErrorMessage):
+    try:
+        i_MethodPointer()
+    except:
+        print(i_ErrorMessage)
 
-pdfBooksFolder = PDFBooksFolder(r"F:\Python Projects\MLProject_OOP\pdf books")
-pdfBooksFolder.StartImageProcessingOnPDFBook()
 
+if len(sys.argv) > 1:
+    pdfBooksFolderPath = sys.argv[1]
+    pdfBooksFolder = PDFBooksFolder(pdfBooksFolderPath)
+    performAMethodInTryCatchBlock(pdfBooksFolder.StartImageProcessingOnPDFBook, constErrorMessage)
 
-#pythonImageClusteringComponent = ImageClusteringComponent(r"C:\Users\mega5\Desktop\python book1\book1")
-#pythonImageClusteringComponent.StartImageClustering()
-finishSound()
+    # maybe i should find another way to write this
+    if len(sys.argv) == 3:
+        imageClusteringComponent = ImageClusteringComponent(pdfBooksFolderPath, i_CategoriesNumber=sys.argv[2])
+    else:
+        imageClusteringComponent = ImageClusteringComponent(pdfBooksFolderPath)
+
+    performAMethodInTryCatchBlock(imageClusteringComponent.StartImageClustering, constErrorMessage)
+    finishSound()
+else:
+    print("Please enter a pdf books folder path")
