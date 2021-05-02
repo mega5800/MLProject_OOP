3cf023346701f6c83d071cc6a4ceca94847b4b99
mega5800 <43865655+mega5800@users.noreply.github.com>
2021-05-03 01:02:42 +0300

    created SilhouetteClusteringComponent.py and added multithreading

--- ImageClusteringComponents/PDFBookClusteringComponent.py
+++ ImageClusteringComponents/PDFBookClusteringComponent.py
@@ -1,41 +1,24 @@
 class PDFBookClusteringComponent:
     def __init__(self, i_RootFolderPath):
-        self.__m_SilhouetteValueCalculator = SilhouetteValueCalculator()
         self.__m_RootFolderPath = i_RootFolderPath
         self.__m_SubFoldersInRootFolderPathCounter = len(glob.glob(self.__m_RootFolderPath + "/*/")) + 1
-        self.__m_PageImageClusteringComponentList = []
+        self.__m_SilhouetteClusteringComponentList = []
         self.__m_ThreadManager = ThreadManager()
-        self.__startImageClusteringOnPDFBook()
-        self.__m_CategoriesNumber = 0
 
-    def __startImageClusteringOnPDFBook(self):
+        self.__preformSilhouetteCalculation()
+
+    def __preformSilhouetteCalculation(self):
         for subFolderIndex in range(1, self.__m_SubFoldersInRootFolderPathCounter):
             self.__m_CurrentPageImageFolder = self.__m_RootFolderPath + r"\page{0}".format(subFolderIndex)
-            self.__m_CategoriesNumber = self.__m_SilhouetteValueCalculator.CalculateSilhouetteValue(self.__m_CurrentPageImageFolder)
-
-            # delete after test
-            print("path = {}, silhouette value = {}".format(self.__m_CurrentPageImageFolder, self.__m_CategoriesNumber))
-            # delete after test
-
-            self.__createResultsFolderAndCategoryFolders(subFolderIndex)
-            self.__m_ThreadManager.AddNewThreadToThreadsList(threading.Thread(target=self.__addNewPageImageClusteringComponentToPageImageClusteringComponentList))
+            self.__m_ThreadManager.AddNewThreadToThreadsList(threading.Thread(target=self.__addNewSilhouetteClusteringComponentToSilhouetteClusteringComponentList, args=(self.__m_CurrentPageImageFolder,subFolderIndex,)))
 
         self.__m_ThreadManager.PerformJoinFunctionOnThreadsList()
 
-    def __addNewPageImageClusteringComponentToPageImageClusteringComponentList(self):
-        self.__m_PageImageClusteringComponentList.append(PageImageClusteringComponent(self.__m_CurrentPageImageFolder, self.__m_CurrentPageImageResultFolder, self.__m_CategoriesNumber))
+    def __addNewSilhouetteClusteringComponentToSilhouetteClusteringComponentList(self, i_PageImageFolder, i_FolderIndex):
+        self.__m_SilhouetteClusteringComponentList.append(SilhouetteClusteringComponent(i_PageImageFolder, i_FolderIndex))
 
-    def __createResultsFolderAndCategoryFolders(self, i_PageIndex):
-        self.__m_CurrentPageImageResultFolder = self.__m_RootFolderPath + r"\page{0} results".format(i_PageIndex)
-        Utils.CreateFolder(self.__m_CurrentPageImageResultFolder)
 
-        for i in range(1, self.__m_CategoriesNumber + 1):
-            Utils.CreateFolder(self.__m_CurrentPageImageResultFolder + r"\category = {0}".format(i))
-
+from Utilities.ThreadManager import ThreadManager
+from ImageClusteringComponents.SilhouetteClusteringComponent import SilhouetteClusteringComponent
 import glob
 import threading
-
-from ImageClusteringComponents.PageImageClusteringComponent import PageImageClusteringComponent
-from ImageClusteringComponents.SilhouetteValueCalculator import SilhouetteValueCalculator
-from Utilities.ThreadManager import ThreadManager
-from Utilities.Utils import Utils
