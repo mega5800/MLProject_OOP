b776557122188054c930f92e29b10f66f64c77e0
mega5800 <43865655+mega5800@users.noreply.github.com>
2021-05-05 23:47:21 +0300

    changed the PDFBooksFolderClusteringComponent.py from multi thread to single thread

--- ImageClusteringComponents/PDFBooksFolderClusteringComponent.py
+++ ImageClusteringComponents/PDFBooksFolderClusteringComponent.py
@@ -7,6 +7,7 @@ class PDFBooksFolderClusteringComponent:
         pdfBooksInPDFBooksFolderCounter = len(glob.glob(self.__m_PDFBooksFolderPath + "/*/")) + 1
 
         for currentPDFBookIndex in range(1, pdfBooksInPDFBooksFolderCounter):
+            print("\nCurrent page is page {}".format(currentPDFBookIndex))
             currentPDFBookPath = self.__m_PDFBooksFolderPath + r"\book{0}".format(currentPDFBookIndex)
             self.__m_PDFBookClusteringComponentList.append(PDFBookClusteringComponent(currentPDFBookPath))
 
